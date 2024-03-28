2292e41107f9582c689dcafc97ab62aa7ce12b64
oanagrecu <oanagrecu09@yahoo.com>
2024-03-28 17:10:50 +0100

    adding services modifications

--- content-section1.php
+++ content-section1.php
@@ -1,18 +1,18 @@
-<div class="section1 p-3 m-0 border-0 bd-example m-0 border-0">
-    <div class="row row-cols-1 row-cols-md-2 g-5 m-4" id="section1-content">
+<div class="section1 p-2 m-0 border-0 bd-example m-0 border-0">
+    <div class="row row-cols-1 row-cols-md-2 g-4 m-4" id="section1-content">
         <div class="col">
             <div class="card h-100 text-center" id="services-card">
                 <h1 class="card-title text-center p-3">Online Store</h1>
                 <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/Hoodie.png"
-                    alt="Hoodie image" class="mx-auto" width="50%" height="auto" style="color: black;">
+                    alt="Hoodie image" class="mx-auto" width="fit-content" height="300px" style="color: black;">
                 <div class="card-body text-center">
                     <h4 class="card-title">Empowering Change through Commerce:</h4>
-                    <p class="card-text ">Every purchase you make contributes directly to supporting the vital
+                    <p class="card-text">Every purchase you make contributes directly to supporting the vital
                         conservation
                         efforts of the Big Life Foundation.</p>
                 </div>
-                <div class="text-center mb-5">
-                    <a href="https://akwanza.teemill.com" class="btn btn-lg btn-shop">Shop to Support</a>
+                <div class="text-center mb-4">
+                    <a href="https://akwanza.teemill.com" class="btn btn-lg btn-shop-1">Shop to Support</a>
                 </div>
             </div>
         </div>
@@ -20,14 +20,15 @@
             <div class="card h-100 text-center" id="services-card">
                 <h1 class="card-title text-center p-3">Reports</h1>
                 <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/reports.png"
-                    alt="hand holding a smartphone" class="mx-auto" width="370px" height="415px" style="color: black;">
+                    alt="hand holding a smartphone" class="mx-auto" width="fit-content" height="300px"
+                    style="color: black;">
                 <div class="card-body text-center">
                     <h4 class="card-title">Visualizing Impact:</h4>
                     <p class="card-text">By transforming complex information into compelling visual narratives,
                         fostering a
                         deeper understanding of conservation challenges and opportunities.</p>
                 </div>
-                <div class="text-center mb-5">
+                <div class="text-center mb-4">
                     <a href="#" class="btn btn-lg btn-reports" data-bs-toggle="modal"
                         data-bs-target="#staticBackdrop">Coming Soon</a>
                     <div class="modal" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
@@ -44,7 +45,9 @@
                                 <div class="modal-body text-start">
                                     <h4>We are building:</h4>
                                     <ul>
-                                        <li>A mosaic of images capturing The Big Life Foundation's rangers in action,
+                                        <li>A mosaic of images capturing <a
+                                                href="https://biglife.org/program-updates/operational-reports"
+                                                target="_blank">The Big Life Foundation</a>'s rangers in action,
                                             community
                                             smiles, and thriving wildlife.</li>
                                         <li>Infographics with symbols representing animals saved, rangers employed, and
