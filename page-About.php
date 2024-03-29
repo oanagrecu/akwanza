f2f72434f86dc96ea717895b2c60b0b005936b48
Walter Clayton <walter.h.clayton@gmail.com>
2024-03-29 11:03:35 +0100

    AK-80

--- page-About.php
+++ page-About.php
@@ -15,27 +15,7 @@
         </head>
 
         </html>
-        <?php include 'content-section3.php'; ?>
-
-        <?php
-        if (have_posts()):
-            while (have_posts()):
-                the_post();
-                ?>
-                <article <?php post_class(""); ?>>
-
-                </article><!-- #post-## -->
-                <?php
-                // If comments are open or we have at least one comment, load up the comment template.
-                if (comments_open() || get_comments_number()):
-                    comments_template();
-                endif;
-            endwhile;
-            the_posts_navigation();
-        else:
-            get_template_part('content-none');
-        endif;
-        ?>
+        <?php include 'content-story.php'; ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
