7811a85611b7f782189b9f714f679e7e7de879fd
Walter Clayton <walter.h.clayton@gmail.com>
2024-03-29 11:03:24 +0100

    AK-80

---
+++ content-story.php
@@ -0,0 +1,120 @@
+<section class="py-8" style="background-color: #DEB693">
+    <div>
+        <div class="row justify-content-around">
+            <div class="col-lg-12 text-center">
+                <H3 class="display-3 fw-bold colonna-mt">Story</H3>
+            </div>
+            <div class="col-sm-10 col-lg-5 mb-3 ">
+                <div id="carouselExampleCaptions" class="carousel slide">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item h-500 w-100 active">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/lioness.png" class="d-block  story-img w-inherit rounded" alt="akwanza lioness inspiration">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The lioness picture</h5>
+                                <p>This was the pose I wanted to convey.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item h-500 w-100">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/kwanza_first_version.png" class="d-block  story-img w-inherit rounded" alt="akwanza first version on a white board">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The first version</h5>
+                                <p>I attempted to replicate the lioness on my white board.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item h-500 w-100">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/kwanza_second_version_print_shop.png" class="d-block  story-img w-inherit rounded" alt="akwanza second version on grid paper">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The second version</h5>
+                                <p>I drew on a grid paper because I wanted a symmetrical body then immediately went to the print shop and told them I wanted a cushion, t-shirt, and poster.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item h-500 w-100">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/kwanza_third_version_with_patterns.png" class="d-block  story-img w-inherit rounded" alt="final version with patterns">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The creative version</h5>
+                                <p>I added patterns to the t-shirts.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item h-500 w-100">
+                            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/kwanza_poster.png" class="d-block  story-img w-inherit rounded" alt="final version with patterns">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>The poster</h5>
+                                <p>The lioness has been staring at me ever since.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            <div class="col-sm-10 col-lg-6">
+                <div class="story-section lead">
+                    <p>
+                        In 2014, my first year of university, an ambassador's passionate pitch convinced me to climb the Roof of Africa and aid children in need.
+                    </p>
+                    <p>
+                        Heroic visions danced in my head.
+                    </p>
+                    <p>
+                        I embarked on a fundraising mission, amassing £3000 for ChildReach. Yet, in a moment of stark awakening, I discovered that a substantial portion of those funds was earmarked for my "adventure."
+                    </p>
+                    <p>
+                        The irony was not lost on me—porters, who struggled for necessities, shouldered our gear up Kilimanjaro's slopes.
+                    </p>
+                    <p>
+                        The dissonance was palpable. The expedition felt far removed from the charitable spirit I yearned to embody—I felt like an imposter.
+                    </p>
+                    <p>
+                        But the experience was transformative. Returning to university, I carried the indomitable spirit of the porters and hosts—hospitality, resilience, and courage became my mantras.
+                    </p>
+                    <p>
+                        From this inspiration, a brand was born.
+                    </p>
+                    <p>
+                        "Kwanza," meaning "first" in Swahili, resonated with me. I envisioned a brand where charity took precedence—no hidden agendas, just unadulterated giving.
+                    </p>
+                    <p>
+                        The lioness captivated me next—her athleticism, duty, and quiet resolve. She became my muse, and I sketched her, poised in a hunting stance, commanding respect rarely afforded to her.
+                    </p>
+                    <p>
+                        My studies in sports science and discovery of the Maasai Olympics—a conservation initiative to protect lions—brought everything full circle. The lioness, "Kwanza," and my aspiration to contribute coalesced into purpose.
+                    </p>
+                    <p>
+                        The path forward was clear, but the vehicle was missing until Teemill, an eco-friendly clothing brand, appeared on the horizon. Their vision aligned with mine, and the plan to sell meaningful apparel was set.
+                    </p>
+                    <p>
+                        Obsession gripped me as I designed apparel adorned with the kwanza lioness—an emblem to wear our "why" with pride. Despite the creative surge, sales and donations remained elusive.
+                    </p>
+                    <p>
+                        Time passed. The kwanza vision, encapsulated in a poster, watched over me—a constant reminder marred by my perfectionism. My designs and website never felt "just right."
+                    </p>
+                    <p>
+                        This year, I cast aside doubts to present the Minimum Viable Product.
+                    </p>
+                    <p>
+                        A slight twist of fate renamed Kwanza to Akwanza—the original name’s multimillion-dollar auction price made the decision easy.
+                    </p>
+                    <p>
+                        Akwanza stands as a testament to pure intentions, to honoring noble deeds, to embodying the lioness' strength in daily life, and to clothing that carries a deeper significance.
+                    </p>
+                    <p class="fw-bold">
+                        The End.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
