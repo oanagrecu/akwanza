79b72d4902236b6b4c4914d030a46e8a74dfabbd
oanagrecu <oanagrecu09@yahoo.com>
2024-03-29 15:35:17 +0100

    fix section2 responsiveness

--- content-section2.php
+++ content-section2.php
@@ -1,24 +1,30 @@
-<div class="section2 p-3 m-0 border-0 m-0 border-0 mb-4">
-    <div class="row row-cols-1 row-cols-md-2 g-5 m-4" id="section2-content">
-        <div class="col">
-            <div class="card w-85 border border-0 h-70 text-center" id="services-card-2">
+<div class="section2 p-2 border-0 mb-4">
+    <h1 class="card-title text-center mx-auto mt-3" id="section2-title">Mission</h1>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-3" id="section2-content">
+        <div class="col-md-6">
+            <div class="card border border-0 h-90 pt-2" id="services-card-2">
                 <div class="card-body">
                     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/MissionLogo.png"
-                        alt="hand holding a smartphone">
+                        alt="mission logo" max-width="100%" max-height="100%">
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card border border-0 h-70 pt-4" id="services-card-3">
-                <h2 class="card-title text-start ">Mission</h2>
-                <div class="card-body text-justify mb-5">
-                    <p class="card-text ">At Akwanza, we harness the spirit of the lioness to empower and protect
-                        our natural
-                        world. Every purchase fuels our mission to support The Big Life Foundation, ensuring that every
-                        dollar goes directly towards safeguarding wildlife and their habitats. We believe in creating a
-                        future where commerce and conservation walk hand in hand, inspiring actions that make a real
-                        difference. Join us in weaving the strength of conservation into every purchase, as we strive to
+        <div class="col-md-6">
+            <div class="card border border-0 h-90 pt-2" id="services-card-3">
+
+                <div class="card-body mission-section">
+                    <p class="card-text">At Akwanza, we harness the spirit of the lioness to empower and protect
+                        our natural world.</p>
+                    <p class="card-text">Every purchase fuels our mission to support The Big Life Foundation, ensuring
+                        that every
+                        dollar goes directly towards safeguarding wildlife and their habitats.</p>
+                    <p class="card-text">We believe in creating a future where commerce and conservation walk hand in
+                        hand, inspiring actions that make a real
+                        difference.</p>
+                    <p class="card-text">Join us in weaving the strength of conservation into every purchase, as we
+                        strive to
                         leave a legacy of balance and beauty for the planet.</p>
+
                 </div>
             </div>
         </div>
